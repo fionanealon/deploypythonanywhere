7273f9639e953542398b08413a451ef5512a0af5
Fiona Nealon <fionanealon2000@yahoo.co.uk>
2020-01-05 21:09:15 +0000

    Database

--- createdatabase.py
+++ createdatabase.py
@@ -1,9 +1,11 @@
 import mysql.connector
+import dbconfig as cfg
 
 db = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="Data2019"
+    host=       cfg.mysql['host'],
+    user=       cfg.mysql['user'],
+    password=   cfg.mysql['password'],
+    database=   cfg.mysql['database'],
 )
 
 cursor = db.cursor()
